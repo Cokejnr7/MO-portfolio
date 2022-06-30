68c8f7dfee32d0ad0a948bb12ec6063afc6bb857
Kolawole Daniel <kolawolecoke@gmail.com>
2022-06-30 11:00:32 +0100

    devon
    added devon and made some changes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-document.addEventListener('mousemove', showCoords)
+// document.addEventListener('mousemove', showCoords)
 
 
 
-function showCoords(event) {
-    var x = event.clientX;
-    var y = event.clientY;
+// function showCoords(event) {
+//     var x = event.clientX;
+//     var y = event.clientY;
 
-    var coords = "X coords: " + x + ", Y coords: " + y;
-    document.getElementById("demo").innerHTML = coords;
-}
+//     var coords = "X coords: " + x + ", Y coords: " + y;
+//     document.getElementById("demo").innerHTML = coords;
+// }
+
+
+// document.addEventListener('click', darkMode);
+// let mode = document.querySelector(".mode");
+// function darkMode(e) {
+//     e.preventDefault();
+
+// }
+
